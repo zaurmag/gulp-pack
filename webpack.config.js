8723cbdba3e2f9c57c404d832bebf58e2c3b218a
Заур <zaurmag411@gmail.com>
2020-10-06 11:57:55 +0300

    Создал webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'scripts.js',
+    },
+    devtool: 'source-map'
+};
+
+if (NODE_ENV == 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings:       false,
+                drop_console:   true,
+                unsafe:         true
+            }
+        })
+    );
+}
