7540ef84d6f3dcab7f4ebba364c36caab60422cc
Заур <zaurmag411@gmail.com>
2020-10-07 23:15:26 +0300

    Подключение webpack config через глобальный объект $.

--- gulp/js.js
+++ gulp/js.js
@@ -1,30 +1,12 @@
-// let path = require('./path/path.js');
-let webpackConfig = require('../webpack.config');
-
 module.exports = function () {
 	return $.gulp.task('js:build', (done) => {
-		$.gulp.src(webpackConfig.entry)
+		$.gulp.src($.webpackConfig.entry)
 			.pipe($.plugins.plumber())
-			.pipe($.webpackStream(webpackConfig))
-			.pipe($.gulp.dest(webpackConfig.output.path)
+			.pipe($.webpackStream($.webpackConfig))
+			.pipe($.gulp.dest($.webpackConfig.output.path)
 				.pipe($.browserSync.reload({
 					stream: true,
 				})));
 		done();
 	});
-
-	// return $.gulp.task('js:build', (done) => {
-	// 	$.gulp.src([path.path.src.js])
-	// 		.pipe($.plugins.changed(path.path.build.js))
-	// 		.pipe($.plugins.plumber())
-	// 		.pipe($.plugins.concat('scripts.js'))
-	// 		.pipe($.gulp.dest(path.path.build.js))
-	// 		.pipe($.plugins.uglify())
-	// 		.pipe($.plugins.rename('scripts.min.js'))
-	// 		.pipe($.gulp.dest(path.path.build.js))
-	// 		.pipe($.browserSync.reload({
-	// 			stream: true,
-	// 		}));
-	// 	done();
-	// });
 };
