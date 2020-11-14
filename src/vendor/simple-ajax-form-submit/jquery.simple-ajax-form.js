e8875ce4631e0598e6fb3120bddf67c4c0b910ca
Заур <zaurmag411@gmail.com>
2020-11-15 00:14:59 +0300

    Редактирование библ. simple-ajax-form: добавил новые возможности

--- src/vendor/simple-ajax-form-submit/jquery.simple-ajax-form.js
+++ src/vendor/simple-ajax-form-submit/jquery.simple-ajax-form.js
@@ -1,96 +1,180 @@
-/* Ajax SimpleSubmit Form Plugin
-   ========================================================================= */
-(function($) {
-    $.fn.simpleSendForm = function(options) {
-        // Options
-        options = $.extend({
-            successTitle: "Спасибо, что выбрали нас!",
-            successText: "Мы свяжемся с Вами в ближайшее время.",
-            errorTitle: "Сообщение не отправлено!",
-            errorSubmit: "Ошибка отправки формы!",
-            errorNocaptcha: "Вы не заполнили каптчу",
-            errorCaptcha: "Вы не прошли проверку каптчи",
-            mailUrl: "../form-submit/submit.php",
-            autoClose: false,
-            autoCloseDelay: 5000,
-            debug: false,
-            captcha: false,
-            captchaPublicKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
-        }, options);
+/**
+ * Simple Ajax form
+ * @author: Zaur Magomedov
+ */
 
-        if(options.captcha) {
-            window.onload = function () {
-                var addScriptCaptcha = document.createElement('script');
-                addScriptCaptcha.src = 'https://www.google.com/recaptcha/api.js';
-                document.body.appendChild(addScriptCaptcha);
-            };
-        }
+(function ($) {
+	$.fn.simpleSendForm = function (options, beforeSubmit, callback) {
+		// Options
+		options = $.extend({
+			successTitle: 'Спасибо, что выбрали нас!',
+			successText: 'Мы свяжемся с Вами в ближайшее время.',
+			errorTitle: 'Сообщение не отправлено!',
+			errorSubmit: 'Ошибка отправки формы!',
+			errorNocaptcha: 'Вы не заполнили каптчу',
+			errorCaptcha: 'Вы не прошли проверку каптчи',
+			mailUrl: '../form-submit/submit.php',
+			autoClear: true,
+			autoClose: false,
+			autoCloseDelay: 5000,
+			debug: false,
+			captcha: false,
+			captchaPublicKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+			successMarkup: false,
+			successInPopup: false,
+		}, options);
 
-        // Submit function
-        var make = function() {
-            var $this = $(this),
-                form = $this.find('.form'),
-                btn = $this.find('.btn-submit'),
-                captcha = $this.find('.recaptcha');
+		if (options.captcha) {
+			window.onload = function () {
+				let addScriptCaptcha = document.createElement('script');
+				addScriptCaptcha.src = 'https://www.google.com/recaptcha/api.js';
+				document.body.appendChild(addScriptCaptcha);
+			};
+		}
 
-            if(options.captcha) {
-                captcha.append('<div class="g-recaptcha" data-sitekey="' + options.captchaPublicKey + '"></div>')
-            }
+		// Submit function
+		let make = () => {
+			const $this = $(this);
+			const id = $this.attr('id');
+			const btn = $this.find('.btn-submit--js');
+			const captcha = $this.find('.recaptcha');
+			const hideSuccess = $this.find('.form__hide-success');
 
-            $(this).submit(function() {
-                var data = $(this).serialize();
-                function errorRes(errorMessage) {
-                    btn.removeClass('progress-bar-animated progress-bar-striped bg-success');
-                    $this.append('<div class="form__error alert alert-danger text-center mt-3 mb-0">' + errorMessage + '</div>');
-                    setTimeout(function() {
-                        $this.find('.form__error').remove();
-                    }, 5000);
-                }
-                $.ajax({
-                    url: options.mailUrl,
-                    type: "POST",
-                    data: data,
-                    beforeSend: function() {
-                        btn.addClass('progress-bar-animated progress-bar-striped bg-success');
-                    },
-                    success: function(res) {
-                        if (res == 1) {
-                            $this[0].reset();
-                            if(options.captcha) {
-                                grecaptcha.reset();
-                            }
-                            $this.find('.form__hide-success').slideUp().delay(5000).slideDown();
-                            btn.removeClass('progress-bar-animated progress-bar-striped bg-success');
-                            $this.find('.form__hide-success').after('<div class="form__sys-message alert alert-success text-center mb-0"></div>');
-                            $this.find('.form__sys-message').html('<h4 class="form__success-title alert-heading">' + options.successTitle + '</h4><p class = "form__success-text" >' + options.successText + '</p>');
-                            setTimeout(function() {
-                                $this.find('.form__sys-message').fadeOut().delay(3000).remove();
-                                if (options.autoClose) {
-                                    $.magnificPopup.close();
-                                }
-                            }, options.autoCloseDelay);
-                        } else if (res == 2) {
-                            errorRes(options.errorNocaptcha);
-                        } else if (res == 3) {
-                            errorRes(options.errorCaptcha);
-                        } else {
-                            errorRes(options.errorSubmit);
-                        }
-                        if(options.debug) {
-                            console.log(res);
-                        }
-                    },
-                    error: function(res) {
-                        errorRes(options.errorSubmit);
-                        if(options.debug) {
-                            console.log(res);
-                        }
-                    }
-                });
-                return false;
-            });
-        };
+			if (options.captcha) {
+				captcha.append(`<div class="g-recaptcha" data-sitekey="${options.captchaPublicKey}"></div>`);
+			}
 
-        return this.each(make);
-    };
+			$(this).submit(function (event) {
+				let data = $(this).serialize();
+				let btnClass = 'progress-bar-animated progress-bar-striped bg-success text-white';
+				let phone = $this.find('input[type="tel"]');
+
+				function errorRes(errorMessage) {
+					btn.removeClass(btnClass);
+					$this.append(`<div class="form__error alert alert-danger text-center mt-3 mb-0">${errorMessage}</div>`);
+					setTimeout(() => {
+						$this.find('.form__error').remove();
+					}, 5000);
+				}
+
+				if ($this[0].checkValidity() === false) {
+					$this.addClass('was-validated');
+					errorRes('Пожалуйста, заполните обязательные поля формы!');
+
+					return false;
+				}
+
+				if (phone.length && !phone.hasClass('is-complete')) {
+					$this.addClass('was-validated');
+					errorRes('Введите корректный номер телефона!');
+
+					return false;
+				}
+
+				const successTemplate = () => {
+					return `
+						<h4 class="form__success-title">${options.successTitle}</h4>
+						<p class = "form__success-text" >${options.successText}</p>\t
+					`;
+				};
+
+				$.ajax({
+					url: options.mailUrl,
+					type: 'POST',
+					data,
+					beforeSend() {
+						btn.addClass(btnClass);
+						if (beforeSubmit) {
+							beforeSubmit();
+						}
+					},
+					success(result) {
+						if (result == 1) {
+							if (!options.autoClear) {
+								$this[0].reset();
+							}
+
+							if (options.captcha) {
+								grecaptcha.reset();
+							}
+							$this.removeClass('was-validated');
+
+							let templ = options.successMarkup || successTemplate;
+
+							if (options.autoClear) {
+								hideSuccess.addClass('is-hidden');
+							} else {
+								hideSuccess.addClass('is-inactive').delay(500).addClass('is-hidden');
+							}
+
+							hideSuccess.after(`<div id="success-${id}" class="form__sys-message" />`);
+							const sysMessage = $this.find('.form__sys-message');
+							sysMessage.append(templ);
+
+							if (options.successInPopup) {
+								sysMessage.addClass('mfp-hide mfp-white-block');
+
+								setTimeout(() => {
+									$.magnificPopup.open({
+										items: {
+											src: `#success-${id}`, // может быть HTML строкой, jQuery объектом, или CSS селектором
+										},
+										type: 'inline',
+										removalDelay: 160,
+										mainClass: 'mfp-scale',
+										callbacks: {
+											close: () => {
+												hideSuccess.removeClass('is-inactive is-hidden');
+											},
+										},
+									});
+								}, 100);
+							}
+
+							setTimeout(() => {
+								sysMessage.addClass('is-active');
+							}, 90);
+
+							if (options.autoClear) {
+								setTimeout(() => {
+									sysMessage.fadeOut().delay(3000).remove();
+									hideSuccess.removeClass('is-hidden');
+								}, 5000);
+							}
+
+							btn.removeClass(btnClass);
+
+							if (options.autoClose) {
+								setTimeout(() => {
+									$.magnificPopup.close();
+								}, options.autoCloseDelay);
+							}
+							if (callback) {
+								callback(event);
+							}
+						} else if (result == 2) {
+							errorRes(options.errorNocaptcha);
+						} else if (result == 3) {
+							errorRes(options.errorCaptcha);
+						} else {
+							errorRes(options.errorSubmit);
+						}
+						if (options.debug) {
+							console.log(result);
+						}
+					},
+					error(result) {
+						errorRes(options.errorSubmit);
+						if (options.debug) {
+							console.log(result);
+						}
+					},
+				});
+
+				return false;
+			});
+		};
+
+		return this.each(make);
+	};
 })(jQuery);
