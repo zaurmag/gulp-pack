8e1acf54af43867bde56c4e4a8094995a449593e
Заур <zaurmag411@gmail.com>
2020-11-15 00:16:24 +0300

    Редактирование компонента form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,30 +1,44 @@
-jQuery(document).ready(function($) {
-    /**
+jQuery(document).ready(($) => {
+	/**
+     * Form init
+     */
+	// Callback form
+	$('#callbackForm').simpleSendForm({
+		autoClose: true,
+		// mailUrl: '../newquiz/form-submit/submit.php'
+
+	}, () => {
+		console.log(' Событие перед отпрвкой формы ');
+	}, () => {
+		console.log(' Событие после отправки формы ');
+	});
+
+	/**
      * Focus and blur on the input field
      */
-    var formInput = $('.form__control');
+	let formInput = $('.form__control');
 
-    function addClassFn(object) {
-        object.parents('.form__group').addClass('is-focused');
-    }
+	function addClassFn(object) {
+		object.parents('.form__group').addClass('is-focused');
+	}
 
-    function remClassFn(object) {
-        object.parents('.form__group').removeClass('is-focused');
-    }
+	function remClassFn(object) {
+		object.parents('.form__group').removeClass('is-focused');
+	}
 
-    formInput
-        .on('focus', function() {
-            addClassFn($(this));
-        })
-        .on('blur', function() {
-            if($(this).val() === '') {
-                remClassFn($(this));
-            }
-        });
+	formInput
+		.on('focus', function () {
+			addClassFn($(this));
+		})
+		.on('blur', function () {
+			if ($(this).val() === '') {
+				remClassFn($(this));
+			}
+		});
 
-    formInput.each(function(){
-        if($(this).val()) {
-            addClassFn($(this));
-        }
-    });
+	formInput.each(function () {
+		if ($(this).val()) {
+			addClassFn($(this));
+		}
+	});
 });
---
+++ src/resources/form-submit/submit.php
@@ -0,0 +1,109 @@
+<?php
+$formData = $_POST;
+$formGo = $formData["go"]; ?>
+
+<?php if(isset($formData)) {
+
+    // ===== Reference ============================
+    $recaptchaOn = false;
+
+    if ($recaptchaOn) {
+        $recaptcha = $_POST['g-recaptcha-response'];
+    }
+
+    /**
+     * Vars
+     */
+    $to = "demo@zaurmag.ru"; // E-mail на который присылать письмо
+    $fromEmail = "no-reply@zaurmag.ru"; // E-mail от имени которого приходит письмо. Почта на домене сайта.
+
+    switch ($formGo) {
+        case 'opt':
+            $subject = "Заявка на прайс-лист для оптовика с сайта imperiya-holoda.ru";
+            break;
+
+        default:
+            $subject = "Сообщение с сайта imperiya-holoda.ru";
+            break;
+    }
+
+    function adopt($text) {
+        return '=?UTF-8?B?'.base64_encode($text).'?=';
+    }
+
+    /**
+     * Message body
+     */
+    $message  = '<html><body>';
+    $message .= "<table>";
+    $title = array(
+        "name" => "Имя:",
+        "phone" => "Телефон:",
+        "email" => "E-mail:",
+        "message" => "Текст сообщения:",
+        "address" => "Адресс торговой точки"
+    );
+
+    foreach($formData as $key => $value) {
+        if ($key != 'go') {
+            $message .= "<tr>";
+            $message .= "<td>";
+            $message .= "<strong>" . $title[$key] . "</strong>";
+            $message .= "</td>";
+            $message .= "<td style='padding-left:12px;'>";
+            $message .= "$value";
+            $message .= "</td>";
+            $message .= "</tr>";
+        }
+    }
+
+    $message .= "</table><br><br>";
+    $message .= '</body></html>';
+    $headers = "MIME-Version: 1.0" . PHP_EOL .
+               "Content-Type: text/html; charset=utf-8" . PHP_EOL .
+               'From: '.adopt($formData["name"]).' <'.$fromEmail.'>' . PHP_EOL .
+               'Reply-To: '.adopt($formData["name"]).' <'.$formData["email"].'> ' . PHP_EOL;
+
+    if ($recaptchaOn) {
+        if (!empty($recaptcha)) {
+            $secret = '6LfMJSgTAAAAABw4lECZsLP5krXztMRZC0_Fgt3O';
+            $url = "//www.google.com/recaptcha/api/siteverify?secret=".$secret ."&response=".$recaptcha."&remoteip=".$_SERVER['REMOTE_ADDR'];
+
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
+                CURLOPT_POST => 1,
+                CURLOPT_POSTFIELDS => array(
+                    'secret' => $secret,
+                    'response' => $recaptcha
+                )
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+
+            $response = json_decode($response, true);
+
+            if ( $response["success"] === false ) {
+                $answer = '2';
+            } else {
+                if (mail($to, adopt($subject), $message, $headers)) {
+                    $answer = '1';
+                } else {
+                    $answer = '0';
+                }
+            }
+
+        } else {
+            $answer = '3';
+        }
+    } else {
+        if (mail($to, adopt($subject), $message, $headers)) {
+                $answer = '1';
+            } else {
+                $answer = '0';
+        }
+    }
+
+    die($answer);
+} ?>
