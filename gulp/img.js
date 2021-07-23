b1f82863d3372cd246f94cf10128e24e931f4ce3
Заур <zaurmag411@gmail.com>
2021-07-23 09:22:52 +0300

    Refactor: rename dist path in root

--- gulp/img.js
+++ gulp/img.js
@@ -1,11 +1,13 @@
 let path = require('./path/path.js');
 module.exports = function () {
 	$.gulp.task('img:build', (done) => {
+		console.log(process.env.TINYPNG_KEY_API);
+
 		return $.gulp.src([path.path.src.img, path.path.src.imgComp])
 			.pipe($.plugins.changed(path.path.build.img))
 			.pipe($.plugins.plumber())
-		// .pipe($.plugins.tinypng('ключ API'))
-			.pipe($.plugins.webp()) // Options - https://github.com/imagemin/imagemin-webp#imageminwebpoptions
+			// .pipe($.plugins.tinypng(process.env.TINYPNG_KEY_API))
+			.pipe($.plugins.webp()) // Options: https://github.com/imagemin/imagemin-webp#imageminwebpoptions
 			.pipe($.gulp.dest(path.path.build.img));
 		done();
 	});
--- gulp/resources.js
+++ gulp/resources.js
@@ -4,7 +4,7 @@ module.exports = function () {
 	$.gulp.task('resources:build', (done) => {
 		return $.gulp.src(path.path.src.resources)
 			.pipe($.plugins.changed(path.path.build.html))
-			.pipe($.gulp.dest(path.path.build.html));
+			.pipe($.gulp.dest(path.path.build.root));
 		// eslint-disable-next-line no-unreachable
 		done();
 	});
