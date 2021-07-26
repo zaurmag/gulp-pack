02b9f8c9030e23894ab27965d9847179444e3961
Заур <zaurmag411@gmail.com>
2021-07-26 17:28:31 +0300

    Refactor: Plugin Go top on clean JS

--- src/vendor/go-top/go-top.js
+++ src/vendor/go-top/go-top.js
@@ -1,27 +1,48 @@
-(function ($) {
-	// eslint-disable-next-line func-names
-	jQuery.fn.scrollToTop = function () {
-		$(this).hide().removeAttr('href')
-		// eslint-disable-next-line eqeqeq
-		if ($(window).scrollTop() != '0') {
-			$(this).fadeIn('slow')
+function scrollTo(to, duration = 700) {
+	const element = document.scrollingElement || document.documentElement
+	const start = element.scrollTop
+	const change = to - start
+	const startDate = +new Date()
+	// t = current time
+	// b = start value
+	// c = change in value
+	// d = duration
+	const easeInOutQuad = (t, b, c, d) => {
+		t /= d / 2
+		if (t < 1) {
+			return c / 2 * t * t + b
 		}
-		let scrollDiv = $(this)
-		$(window).scroll(() => {
-			if ($(window).scrollTop() === '0') {
-				$(scrollDiv).fadeOut('slow')
-			} else {
-				$(scrollDiv).fadeIn('slow')
-			}
-		})
-		$(this).click(() => {
-			$('html, body').animate({
-				scrollTop: 0
-			}, 'slow')
-		})
-	}
-})(jQuery)
+		t--
 
-jQuery(document).ready(($) => {
-	$('.go-top').scrollToTop()
+		return -c / 2 * (t * (t - 2) - 1) + b
+	}
+	const animateScroll = () => {
+		const currentDate = +new Date()
+		const currentTime = currentDate - startDate
+		element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration), 10)
+		if (currentTime < duration) {
+			requestAnimationFrame(animateScroll)
+		} else {
+			element.scrollTop = to
+		}
+	}
+	animateScroll()
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const btn = document.querySelector('.go-top--js')
+	window.addEventListener('scroll', () => {
+		if (pageYOffset > 100) {
+			btn.classList.add('is-show')
+		} else {
+			btn.classList.remove('is-show')
+		}
+	})
+
+	// Click on btn
+	btn.addEventListener('click', event => {
+		event.preventDefault()
+		scrollTo(0, 200)
+	})
 })
+
