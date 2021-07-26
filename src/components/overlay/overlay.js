d7781ba4b8eb429afbd3302dfbf0a5f34449873d
Заур <zaurmag411@gmail.com>
2021-07-26 16:01:32 +0300

    Fix: Overlay

--- src/components/overlay/overlay.js
+++ src/components/overlay/overlay.js
@@ -2,15 +2,16 @@ const body = document.body
 
 export function overlayAdd() {
 	const overlay = document.createElement('div')
+	overlay.setAttribute('data-backdrop', 'overlay')
 	overlay.classList.add('overlay')
 	body.classList.add('is-active')
 	setTimeout(() => body.classList.add('is-animate'), 100)
 	body.appendChild(overlay)
-
-	return overlay
 }
 export function overlayRemove() {
 	const overlay = document.querySelector('.overlay')
-	overlay.remove()
+	if (overlay) {
+		overlay.remove()
+	}
 	body.classList.remove('is-animate', 'is-active')
 }
