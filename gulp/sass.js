37a1775bd4f642f41a6940bfff83bcd67f627883
Заур <zaurmag411@gmail.com>
2021-07-26 16:03:19 +0300

    Fix: Task style

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,22 +1,22 @@
-let path = require('./path/path.js');
+let path = require('./path/path.js')
 const sass = require('gulp-sass')(require('sass'))
 
 module.exports = function () {
 	$.gulp.task('style:build', (done) => {
 		return $.gulp.src(path.path.src.style)
 			.pipe($.plugins.plumber())
-			.pipe($.plugins.if(!$.yargs.minifyCss, $.plugins.sourcemaps.init({largeFile: true})))
+			.pipe($.plugins.if(!$.yargs.minifyCss, $.plugins.sourcemaps.init({ largeFile: true })))
 			.pipe(sass())
 			.pipe($.plugins.pxtorem())
 			.pipe($.plugins.if($.yargs.minifyCss, $.plugins.csso()))
-			.pipe($.plugins.autoprefixer({
+			.pipe($.plugins.if($.yargs.minifyCss, $.plugins.autoprefixer({
 				overrideBrowserslist: ['last 2 versions'],
-				cascade: false,
-			}))
-			.pipe($.plugins.if($.yargs.minifyCss, $.plugins.rename({suffix: '.min'}), $.plugins.sourcemaps.write('.')))
+				cascade: false
+			})))
+			.pipe($.plugins.if($.yargs.minifyCss, $.plugins.rename({ suffix: '.min' }), $.plugins.sourcemaps.write('.')))
 			.pipe($.gulp.dest(path.path.build.style))
 			.pipe($.browserSync.reload({
-				stream: true,
+				stream: true
 			}))
 		done()
 	})
