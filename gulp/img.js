be18fe8e1374b3c937eda4853dc9d66b10199336
zaurmag411@gmail.com <guKGyVmC7o>
2021-12-28 22:07:35 +0300

    Upd: Copying original images to build

--- gulp/img.js
+++ gulp/img.js
@@ -4,6 +4,7 @@ module.exports = function () {
 		return $.gulp.src([path.path.src.img, '!src/images/icons/**/*', path.path.src.imgComp])
 			.pipe($.plugins.changed(path.path.build.img))
 			.pipe($.plugins.plumber())
+			.pipe($.gulp.dest(path.path.build.img)) // Copy all original img files
 			// .pipe($.plugins.tinypng(process.env.TINYPNG_KEY_API))
 			.pipe($.plugins.webp()) // Options WebP: https://github.com/imagemin/imagemin-webp#imageminwebpoptions
 			.pipe($.gulp.dest(path.path.build.img))
