8acf5d2cfe38aff114414fa8c143851070cddcdc
zaurmag411@gmail.com <guKGyVmC7o>
2022-07-12 13:53:57 +0300

    Feat: Delete jQuery
    - replace modal Magnific Popup to bootstrap modal
    - delete Simple ajax form
    - all code with jquery support

--- src/vendor/vendor.js
+++ src/vendor/vendor.js
@@ -1,13 +1,2 @@
 import './go-top/go-top'
-
-// Magnific Popup
-// import './magnific-popup/init'
-
-// Owl Carousel
-// import './owl-carousel/init'
-
-// Simple ajax form
-// import './simple-ajax-form-submit/jquery.simple-ajax-form'
-
-// Fixed block
-// import './fixedblock'
+import './bootstrap/bootstrap'
